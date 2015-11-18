9c2d07cd8f0c2a09f646c93123536aa0511cb42a
Rohan Loomis <roxas240@gmail.com>
2015-11-18 05:49:40 -0500

    Moved Launcher to new Project

--- nerd-shooter-desktop/src/com/xemplar/games/launcher/action/OnButtonPress.java
+++
@@ -1,9 +0,0 @@
-package com.xemplar.games.launcher.action;
-
-import java.awt.event.MouseEvent;
-
-import com.xemplar.games.launcher.LauncherButton;
-
-public interface OnButtonPress {
-	public void onButtonPress(LauncherButton button, MouseEvent e);
-}
